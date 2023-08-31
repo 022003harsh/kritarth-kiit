df9da843cceaf8c6c68a6bed25d5fbfc77c5d4c6
Harsh <harshku2222@gmail.com>
2023-08-31 13:50:28 +0530

    many many changes

--- src/component/Events.jsx
+++ src/component/Events.jsx
@@ -30,7 +30,7 @@ const Events = (props) => {
             <div className='max-width-[505px] max-h-[505px] rounded-xl sm:rounded-3xl bg-black border-solid border-[0.92px] border-[#7F7F7F]'>
               <img src={foundUser.url} alt="" className='rounded-xl sm:rounded-3xl max-width-[500px] w-full max-h-[500px] h-full' />
             </div>
-            <div className='flex flex-col justify-center items-center py-5 px-5 sm:py-10 sm:px-10 max-width-[500px] sm:h-[500px] sm:w-[500px] w-full rounded-xl sm:rounded-3xl  border-solid border-[0.92px] border-[#7F7F7F] blurbox'>
+            <div className='flex flex-col text-justify justify-center items-center py-5 px-5 sm:py-10 sm:px-10 max-width-[500px] sm:h-[500px] sm:w-[500px] w-full rounded-xl sm:rounded-3xl  border-solid border-[0.92px] border-[#7F7F7F] blurbox'>
               <div className='text-white font-Mulish text-2xl font-bold md:leading-[37px] tracking-wide md:tracking-widest self-start'>{foundUser.name}</div>
               <div className='text-white font-Mulish text-lg sm:text-xl italic font-extrabold md:leading-[23px] tracking-normal md:tracking-widest mt-[5px] overflow-x-scroll whitespace-pre-wrap'>{foundUser.message1}</div>
               
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -29,7 +29,7 @@ function Home() {
         <div className='relative pb-10'>
         <div className='-z-40 absolute top-0 left-0 w-full h-full '></div>
         <div className='w-[100vw] '>
-          <div className='max-w-[1920px] h-[80vh] md:h-[90vh] p-3 xl:p-0 flex flex-col items-center justify-center mx-auto'>
+          <div className='max-w-[1920px] min-h-[80vh] md:h-[90vh] p-3 xl:p-0 flex flex-col items-center justify-center mx-auto'>
             <div className='relative  xl:p-0 w-full 2xl:w-[1422px] h-[480px] mx-auto bg-transparent md:mt-[40px]'>
               {/* <img src={img1} alt="Event image" className=' object-fill max-h-[700px] w-full' /> */}
                 
@@ -38,8 +38,6 @@ function Home() {
                 <div className='relative lg:w-[70%] mt-2 md:mt-0 ml-[0px] md:ml-[15px] zoom-in-out-box'>
                     <img src={kritarthlogo} alt="" className='self-start ' />
                     <h1 className='absolute bottom-[0px] right-[25px] lg:bottom-[10px] lg:right-[55px] text-xl md:text-2xl lg:text-4xl text-white leading-3 font-normal font-poppins'>HEARTS OF HOPE</h1>
-                    
-                    
                 </div>
                 
                 <div className=''>
@@ -51,10 +49,10 @@ function Home() {
                     </button>
                 </Link>
                 </div>
-                <div className='w-full md:w-[60%] lg:w-[50%] flex items-center justify-center h-full '>
+                <div className='w-full md:w-[60%] lg:w-[50%] flex items-center text-justify justify-center h-full '>
                   <div className=''></div>
                   <div className='h-full lg:translate-y-[-30px] md:mr-8 group mb-1 md:mb-0 md:px-7 px-2 py-2 md:py-6 lg:py-9 xl:py-10 xl:px-12 rounded-[10px] md:rounded-[20px] flex items-center justify-center bg-black/20 shadow-md backdrop-blur-sm border-solid border-[0.795px] border-[#7F7F7F] italic '>
-                    <div className='text-white  shadow-lg font-semibold text-sm sm:text-base  xl:text-lg md:leading-[27px] tracking-wide md:tracking-widest font-Mulish'>
+                    <div className='text-white  shadow-lg font-semibold text-xs sm:text-sm  xl:text-lg md:leading-[27px] tracking-wide md:tracking-widest font-Mulish'>
                     {/* A forum for everyone to know about their mega event and participate in the super exciting competitions for an overwhelming experience.
 
                     Pace up and register yourself for a magnificent eve! */}
@@ -76,7 +74,7 @@ function Home() {
         <div className='flex md:flex-row flex-col-reverse justify-between gap-4 items-center w-full h-full '>
             <div className='w-full md:w-[60%] 2xl:w-[60%] flex items-center justify-center h-full'>
               <div className='md:ml-8 mb-1  md:mb-0 md:px-7 px-2 py-2 md:py-6 lg:py-9 xl:py-10 xl:px-12 rounded-[10px] md:rounded-[20px] flex items-center justify-center bg-black/20 shadow-md backdrop-blur-sm border-solid border-[0.795px] border-[#7F7F7F] italic'>
-                <div className='text-white font-semibold text-sm sm:text-base xl:text-lg md:leading-[27px] tracking-wide md:tracking-widest font-Mulish'>
+                <div className='text-white text-justify font-semibold text-sm sm:text-base xl:text-lg md:leading-[27px] tracking-wide md:tracking-widest font-Mulish'>
                 Hearts of Hope' depicts multiple hearts yearning for the same thing:
                 the hope of being absorbed into a family or being among loved ones,
                 and most importantly, the hope of being recognised, treasured, and
